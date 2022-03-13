208aa53c76662f2e6779b0d61c2c89744c64cf1b
Sadık <Sadk@SADIK>
2022-03-13 11:35:02 +0300

    Add project files.

---
+++ customer/CustomerManajer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customer
+{   
+
+     class CustomerManajer
+    {
+        static void Main(string[] args) 
+        { 
+        Customer customer1 = new Customer();
+         customer1.Name = "Musa";
+         customer1.Lastname = "Bayram";
+         customer1.Age = 29;
+         customer1.id = "001";
+         
+        Customer customer2 = new Customer();
+         customer2.Name = "Nurten";
+         customer2.Lastname = "Bayram";
+         customer2.Age = 30;
+         customer2.id = "002";
+
+        Customer customer3 = new Customer();
+         customer3.Name = "Mustafa Sadık";
+         customer3.Lastname = "Bayram";
+         customer3.Age = 6;
+         customer3.id = "003";
+            
+
+         Customer[] customers = new Customer[] {customer1,customer2,customer3};
+
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Lastname);
+                Console.WriteLine(customer.Age);
+                Console.WriteLine(customer.id);
+                Console.WriteLine("-------.Next.-------");
+            }
+
+            musterimanajer musterimanajers = new musterimanajer { };
+            musterimanajers.add(customer1);
+            Console.WriteLine("-------.Next.-------");
+            musterimanajers.add(customer2);
+            Console.WriteLine("-------.Next.-------");
+            musterimanajers.add(customer3);
+            Console.WriteLine("-------.Next.-------");
+            musterimanajers.list(customer2);
+            Console.WriteLine("-------.Next.-------");
+            musterimanajers.delete(customer3);
+            
+        }
+
+    }
+}
---
+++ customer/musterimanajer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customer
+{
+     class musterimanajer
+    {
+        public void add(Customer customers)
+        {
+            Console.WriteLine("Tebrikler Yeni Kişi eklediniz." + customers.id);
+        }
+        public void delete(Customer customers)
+        {
+            Console.WriteLine("Kişi Silindi." + customers.id);
+        }
+        public void list(Customer customers)
+        {
+            Console.WriteLine("Kişi Listeye Alındı." + customers.id);
+        }
+    }
+}
